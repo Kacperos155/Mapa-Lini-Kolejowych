00905966315484fe0119ef573be2d3718ae678a0
cola98765 <tpodole@gmail.com>
2022-06-12 06:49:50 +0200

    added electrified lines

--- Mapa lini kolejowych/Website/pl/Legend.js
+++ Mapa lini kolejowych/Website/pl/Legend.js
@@ -32,7 +32,6 @@ function addLegendSpeedLimit(label, color) {
 }
 
 {
-	let lastSpeedLimit = {};
 	let label_prefix = "Limit prędkości" + ": ";
 	for (let i = 0; i < (speedLimits.length - 1); ++i) {
 		let label = label_prefix + speedLimits[i].min_speed + " - " + (speedLimits[i + 1].min_speed - 1) + " km/h";
@@ -57,6 +56,12 @@ const Legend_Markers_Lines = Legend_markers.concat([
 		type: "polyline",
 		weight: 5,
 		color: defaultLineColor
+	},
+	{
+		label: "Linia zelektryfikowana",
+		type: "polyline",
+		weight: 5,
+		color: electrifiedLineColor
 	},
 	{
 		label: "Nieużywane tory",
